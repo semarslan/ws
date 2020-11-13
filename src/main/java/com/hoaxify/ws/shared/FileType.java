aedf766775f50035e8df64f168ae84d952e3725c
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-13 22:41:01 +0300

    hata uyarıları için değişiklikler yapıldı

--- src/main/java/com/hoaxify/ws/shared/FileType.java
+++ src/main/java/com/hoaxify/ws/shared/FileType.java
@@ -1,8 +1,6 @@
 package com.hoaxify.ws.shared;
 
 
-import com.hoaxify.ws.user.UniqueUsernameValidator;
-
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Retention;
@@ -13,11 +11,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({ FIELD }) //Bu annotationı nerde kullanacağımızı belirtiyoruz. Field dediğimiz de id, usename, password gibi alanlar yani
 @Retention(RUNTIME) //annotationın runtimeda çözümlenmesi ile ilgili bir davranış
-@Constraint(validatedBy = { ProfileImageValidator.class })
-public @interface ProfileImage {
-    String message() default "{hoaxify.constraint.ProfileImage.message}";
+@Constraint(validatedBy = { FileTypeValidator.class })
+public @interface FileType {
+    String message() default "{hoaxify.constraint.FileType.message}";
 
     Class<?>[] groups() default { };
 
     Class<? extends Payload>[] payload() default { };
+
+    String[] types();
 }
