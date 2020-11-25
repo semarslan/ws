15f2540e045c3827f8251c4440f8cd13d5166706
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-25 23:21:34 +0300

    schedule işlemi yapıldı.
    File ile iç içe old için file service'e tekrar çekildi.

--- src/main/java/com/hoaxify/ws/file/FileService.java
+++ src/main/java/com/hoaxify/ws/file/FileService.java
@@ -5,6 +5,8 @@ import org.apache.tika.Tika;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,9 +16,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@EnableScheduling
 public class FileService {
 
     AppConfiguration appConfiguration;
@@ -76,5 +80,23 @@ public class FileService {
         fileAttachment.setName(fileName);
         fileAttachment.setDate(new Date());
         return fileAttachmentRepository.save(fileAttachment);
+    }
+
+    /**
+     * 24 saatten eski olan ve
+     * hoaxla ilişkisi bulunmayan dosyaları silen metod.
+     */
+    @Scheduled(fixedRate = 24*60*60*1000 )
+    public void cleanupStorage() {
+        Date twentyFourHourAgo = new Date(System.currentTimeMillis() - (24*60*60*1000)); //24 saatin ms karşılığı
+        List<FileAttachment> filesToBeDeleted = fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(twentyFourHourAgo);
+        for (FileAttachment file : filesToBeDeleted) {
+            // delete file in folder
+            deleteFile(file.getName());
+
+            // delete from file in db
+            fileAttachmentRepository.deleteById(file.getId());
+
+        }
     }
 }
