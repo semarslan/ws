27d6716bd434e0213dccd14fe217d7ab091a8342
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-25 22:24:42 +0300

    upload edilen file db'ye yazıldı. Bunun için model repository oluşturuldu. service ve controller güncellendi.

---
+++ src/main/java/com/hoaxify/ws/file/FileAttachmentRepository.java
@@ -0,0 +1,6 @@
+package com.hoaxify.ws.file;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+}
--- src/main/java/com/hoaxify/ws/file/FileController.java
+++ src/main/java/com/hoaxify/ws/file/FileController.java
@@ -16,8 +16,7 @@ public class FileController {
     FileService fileService;
 
     @PostMapping("/api/1.0/hoax-attachments")
-    Map<String, String> saveHoaxAttachment (MultipartFile file) {
-        String fileName = fileService.saveHoaxAttachment(file);
-        return Collections.singletonMap("name", fileName);
+    FileAttachment saveHoaxAttachment (MultipartFile file) {
+        return fileService.saveHoaxAttachment(file);
     }
 }
