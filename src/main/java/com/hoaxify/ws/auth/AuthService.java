0ade0469462467500d2e429af23d98572581ed23
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-27 16:51:45 +0300

    hatalı usernaem ve ya password girildiğinde gelen cevabın 401 olması içni refactor işlemi ve geliştirme işlemi yapıldı. YEni bir AuthException classı oluşturuldu.

--- src/main/java/com/hoaxify/ws/auth/AuthService.java
+++ src/main/java/com/hoaxify/ws/auth/AuthService.java
@@ -1,6 +1,7 @@
 package com.hoaxify.ws.auth;
 
 import com.hoaxify.ws.user.User;
+import com.hoaxify.ws.user.UserRepository;
 import com.hoaxify.ws.user.UserService;
 import com.hoaxify.ws.user.vm.UserVM;
 import io.jsonwebtoken.Jwts;
@@ -11,26 +12,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
 
-    UserService userService;
+    UserRepository userRepository;
 
     PasswordEncoder passwordEncoder;
 
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
-        this.userService = userService;
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     public AuthResponse authenticate(Credentials credentials) {
-        User inDB = userService.getByUsername(credentials.getUsername());
-        boolean matches = passwordEncoder.matches(credentials.getPassword(), inDB.getPassword());
-        if (matches) {
-            UserVM userVM = new UserVM(inDB);
-            String token = Jwts.builder().setSubject(""+inDB.getId()).signWith(SignatureAlgorithm.HS512, "my-app-secret").compact();
-            AuthResponse response = new AuthResponse();
-            response.setUserVM(userVM);
-            response.setToken(token);
-            return response;
+        User inDB = userRepository.findByUsername(credentials.getUsername());
+        if (inDB == null) {
+            throw new AuthException();
         }
-        return null;
+
+        boolean matches = passwordEncoder.matches(credentials.getPassword(), inDB.getPassword());
+
+        if (!matches) {
+            throw new AuthException();
+        }
+
+        UserVM userVM = new UserVM(inDB);
+        String token = Jwts.builder().setSubject("" + inDB.getId()).signWith(SignatureAlgorithm.HS512, "my-app-secret").compact();
+        AuthResponse response = new AuthResponse();
+        response.setUserVM(userVM);
+        response.setToken(token);
+        return response;
     }
 }
