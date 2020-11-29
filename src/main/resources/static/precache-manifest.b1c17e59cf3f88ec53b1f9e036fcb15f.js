5692015955ddc3ebd526e2f871ea678e9a4912b3
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-29 11:00:15 +0300

    react projesi spring projesine implement edildi ve ./mvnw package komutu ile ikisi birlikte tek bir jar dosyası haline getirildi.
    Bazı hatalar aldım öncelikle bilgisayarımda dockerı kaldırmam gerketi. MAvenı ordan alıyordum. Onu daha sonra çözeceğim.
    Dockerı kaldırıp, mavenı kurdum. Pom.xmlde hatalı bir alan eklemişim onu düzenledim ve jar dosyam oluşmuş oldu.

---
+++ src/main/resources/static/precache-manifest.b1c17e59cf3f88ec53b1f9e036fcb15f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c5c9cb2fcc107e2fcb6feef0d997a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a5f424ee50f9e66a4ca",
+    "url": "/static/css/main.bc87046a.chunk.css"
+  },
+  {
+    "revision": "bc031e8e3daff56fa51e",
+    "url": "/static/js/2.d0a341ee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d0a341ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5f424ee50f9e66a4ca",
+    "url": "/static/js/main.8a1df4d2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "2dcb4821b18b3e6060236b90b13c136b",
+    "url": "/static/media/hoaxify.2dcb4821.png"
+  },
+  {
+    "revision": "926fda932fcddf9dae79b8a3a560745b",
+    "url": "/static/media/profile.926fda93.png"
+  }
+]);
